849c2facea0358e749f37946579b3be7fb555ba0
wchap23 <wchap23@wgu.edu>
2024-11-18 12:15:08 -0500

    Initial Commit with original code from website. -Removed Personal details

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.h-logo').classList.add('show');
+    }, 1000);  // 1 second delay
+});
+
